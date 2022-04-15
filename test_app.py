107a73d54950cb18c47bafe90b2e7d38a2d24eea
Andrea-Rossini <andrea.rossini.brocca@gmail.com>
2022-04-15 14:09:55 +0200

    Git action test, added mul function

--- test_app.py
+++ test_app.py
@@ -1,8 +1,11 @@
-from app import add, subtract
+from app import add, subtract, mul
 
 def test_add():
     assert add(1, 2) == 3
 
 def test_subtract():
     assert subtract(3, 2) == 1
+
+def test_mul():
+    assert mul(3, 2) == 6
 
